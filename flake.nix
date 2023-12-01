23b78be1da871d302708093330c975dedfd8ec0e
Johan Herland <johan@herland.net>
2023-12-01 12:31:16 +0100

    Initial setup with flake.nix and empty requirements.txt

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "jherland's solutions to Advent Of Code 2023";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { nixpkgs, ... }@inputs: {
+    devShells.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        name = "AoC23";
+        buildInputs = with pkgs; [
+          git
+          python312
+          python312Packages.venvShellHook
+        ];
+        venvDir = "./.venv";
+        postShellHook = ''
+          unset SOURCE_DATE_EPOCH
+          pip install --upgrade pip
+          pip install -r ./requirements.txt
+        '';
+      };
+    };
+}
