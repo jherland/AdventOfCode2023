d7ccb1b05126f66e6bd7e9ad77a724770b1e412d
Johan Herland <johan@herland.net>
2023-12-01 13:27:21 +0100

    Day 1

---
+++ 01.py
@@ -0,0 +1,33 @@
+import re
+from typing import Iterable, Iterator
+
+WORDS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+def digits(s: str, *, parse_spelled: bool = False) -> Iterator[int]:
+    digits = {str(d): d for d in range(1, 10)}
+    if parse_spelled:
+        digits |= {word: num for num, word in enumerate(WORDS, start=1)}
+    while s:
+        for k, v in digits.items():
+            if s.startswith(k):
+                yield v
+        s = s[1:]
+
+
+def first_and_last[T](l: Iterable[T]) -> (T, T):
+    it = iter(l)
+    first = last = next(it)
+    for item in it:
+        last = item
+    return first * 10 + last
+
+
+with open("01.input") as f:
+    lines = list(f)
+
+# Part 1: What is the sum of all of the calibration values?
+print(sum(first_and_last(digits(line)) for line in lines))
+
+# Part 2: What is the sum of calibration values (incl. spelled-out digits)?
+print(sum(first_and_last(digits(line, parse_spelled=True)) for line in lines))
