4bcefdb8baa872af8ce6f55548317a56939dd9f2
Johan Herland <johan@herland.net>
2023-12-16 14:23:17 +0100

    test.py: Handle missing .py or .expect files

--- test.py
+++ test.py
@@ -6,20 +6,36 @@ from pathlib import Path
 
 py_files = {p.stem: p for p in Path.cwd().glob("??.py")}
 expect_files = {p.stem: p for p in Path.cwd().glob("??.expect")}
-for stem in sorted(py_files.keys() | expect_files.keys()):
-    print(f"--- {stem} ---")
-    script = py_files[stem]
-    expect = expect_files[stem].read_text()
+
+
+def test(stem: str) -> int:
+    print(f"--- Test day #{stem} ---")
+    try:
+        script = py_files[stem]
+    except KeyError:
+        print(f"Missing Python file for day #{stem}!")
+        return 2
+    try:
+        expect = expect_files[stem].read_text()
+    except KeyError:
+        print(f"Missing expected output for day #{stem}!")
+        return 3
+
     proc = subprocess.run(
         [sys.executable, script],  # noqa: S603
         capture_output=True,
         text=True,
         check=True,
     )
-    if proc.stdout == expect:
-        print(proc.stdout, end="")
-    else:
+    if proc.stdout != expect:
         print(f"*** Test failed for day #{stem}!")
         print(f"    EXPECTED: {expect!r}")
         print(f"     BUT GOT: {proc.stdout!r}")
-        sys.exit(1)
+        return 1
+    print(proc.stdout, end="")
+    return 0
+
+
+for stem in sorted(py_files.keys() | expect_files.keys()):
+    if retval := test(stem):
+        sys.exit(retval)
